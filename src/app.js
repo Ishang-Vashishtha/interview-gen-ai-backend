20c5afddccf84740c443e56f07e67151bb736b9b
Ishang vashishtha <115099296+Ishang-Vashishtha@users.noreply.github.com>
2026-07-07 18:01:30 +0530

    Fix CORS origin URL by removing trailing slash

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://interview-gen-ai-frontend.vercel.app/",
+    origin: "https://interview-gen-ai-frontend.vercel.app",
     credentials: true,
   }),
 );
